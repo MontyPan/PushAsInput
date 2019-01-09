2a6004e46ff7e6261ed812df85451ddc5cb6a0f8
MontyPan <monty.pan@dontcareabout.us>
2019-01-10 00:56:57 +0800

    增加：使用 RequestBuilder 取得資料

--- src/main/java/us/dontcareabout/PushAsInput/client/PushAsInputEP.java
+++ src/main/java/us/dontcareabout/PushAsInput/client/PushAsInputEP.java
@@ -2,9 +2,15 @@ package us.dontcareabout.PushAsInput.client;
 
 import java.util.ArrayList;
 
-import com.google.gwt.core.shared.GWT;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.core.client.JsonUtils;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -13,6 +19,7 @@ import com.sencha.gxt.widget.core.client.container.Viewport;
 import us.dontcareabout.PushAsInput.client.data.DataReadyEvent;
 import us.dontcareabout.PushAsInput.client.data.DataReadyEvent.DataReadyHandler;
 import us.dontcareabout.PushAsInput.client.ui.SingaporeUI;
+import us.dontcareabout.PushAsInput.client.vo.RawPushJS;
 import us.dontcareabout.PushAsInput.shared.RawPush;
 import us.dontcareabout.gwt.client.GFEP;
 
@@ -41,10 +48,10 @@ public class PushAsInputEP extends GFEP {
 	@Override
 	protected void start() {
 		vp.mask("資料讀取中...");
-		fetchData();
+		fetchByRequest();
 	}
 
-	private void fetchData() {
+	private void fetchByRPC() {
 		String url = "https://www.ptt.cc/bbs/Singapore/M.1535806897.A.57F.html";
 		rpc.pushList(url, new AsyncCallback<ArrayList<RawPush>>() {
 			@Override
@@ -58,6 +65,34 @@ public class PushAsInputEP extends GFEP {
 				// TODO Auto-generated method stub
 			}
 		});
+	}
+
+	private void fetchByRequest() {
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, GWT.getHostPageBaseURL() + "Singapore.json");
+
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				@Override
+				public void onResponseReceived(Request request, Response response) {
+					ArrayList<RawPush> result = new ArrayList<>();
+					JsArray<RawPushJS> data = JsonUtils.<JsArray<RawPushJS>>safeEval(response.getText());
+
+					for (int i = 0; i < data.length(); i++) {
+						//必須繞一層，不然泛型會導致 JSNI casting 失敗
+						RawPushJS rpjs = data.get(i);
+						result.add(rpjs);
+					}
+
+					vp.unmask();
+					eventBus.fireEvent(new DataReadyEvent(result));
+				}
+
+				@Override
+				public void onError(Request request, Throwable exception) {
+				}
+			});
+		} catch(Exception e) {
+		}
 	}
 
 	public static HandlerRegistration addDataReady(DataReadyHandler handler) {
