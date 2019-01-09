c0d34d641124cc2bd15f758b3cd02a3c007e2a39
MontyPan <monty.pan@dontcareabout.us>
2019-01-09 20:37:28 +0800

    RPC 版本的「取得推文」

--- src/main/java/us/dontcareabout/PushAsInput/client/RpcService.java
+++ src/main/java/us/dontcareabout/PushAsInput/client/RpcService.java
@@ -1,8 +1,13 @@
 package us.dontcareabout.PushAsInput.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+import us.dontcareabout.PushAsInput.shared.RawPush;
+
 @RemoteServiceRelativePath("RPC")
 public interface RpcService extends RemoteService{
+	ArrayList<RawPush> pushList(String url);
 }
--- src/main/java/us/dontcareabout/PushAsInput/server/RpcServiceImpl.java
+++ src/main/java/us/dontcareabout/PushAsInput/server/RpcServiceImpl.java
@@ -1,11 +1,17 @@
 package us.dontcareabout.PushAsInput.server;
 
-import java.util.Date;
+import java.util.ArrayList;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import us.dontcareabout.PushAsInput.client.RpcService;
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import us.dontcareabout.PushAsInput.shared.RawPush;
 
 public class RpcServiceImpl extends RemoteServiceServlet implements RpcService {
 	private static final long serialVersionUID = 1L;
 
+	@Override
+	public ArrayList<RawPush> pushList(String url) {
+		return Fetch.process(url);
+	}
 }
