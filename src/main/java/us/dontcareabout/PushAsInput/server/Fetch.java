7018ea350b1cea854031d1813869bb2e861260a9
MontyPan <monty.pan@dontcareabout.us>
2019-02-21 23:05:39 +0800

    抓取轉換後自動作 git reset 並 push 至 remote

--- src/main/java/us/dontcareabout/PushAsInput/server/Fetch.java
+++ src/main/java/us/dontcareabout/PushAsInput/server/Fetch.java
@@ -1,12 +1,26 @@
 package us.dontcareabout.PushAsInput.server;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import com.google.gson.Gson;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import us.dontcareabout.PushAsInput.shared.FetchSetting;
 import us.dontcareabout.PushAsInput.shared.RawPush;
 import us.dontcareabout.PushAsInput.shared.RawPushImpl;
 
@@ -34,5 +48,31 @@ public class Fetch {
 		}
 
 		return result;
+	}
+
+	private static final Setting setting = new Setting();
+	private static final FetchSetting singapore = new FetchSetting("https://www.ptt.cc/bbs/Singapore/M.1535806897.A.57F.html", "Singapore.json");
+	private static final SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+
+	public static void main(String[] args) throws Exception {
+		ArrayList<RawPush> result = process(singapore.getUrl());
+		Gson gson = new Gson();
+		Files.write(
+			gson.toJson(result).getBytes(Charsets.UTF_8),
+			new File(setting.repo(), singapore.getFileName())
+		);
+
+		CredentialsProvider cp = new UsernamePasswordCredentialsProvider(setting.username(), setting.password());
+		File local = new File(new File(setting.repo()), ".git");
+		Repository repo = new FileRepository(local);
+		ObjectId oid = repo.resolve("HEAD^");
+
+		Git git = new Git(repo);
+
+		git.reset().setRef(oid.getName()).call();
+		git.add().addFilepattern(".").call();
+		git.commit().setAll(true).setMessage(format.format(new Date())).call();
+		git.push().setCredentialsProvider(cp).setForce(true).call();
+		git.close();
 	}
 }
